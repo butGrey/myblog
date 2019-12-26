93d399a9365e27f8e93551d4133224abe3cf56ef
llbai <40267057+llbai@users.noreply.github.com>
2019-12-26 20:37:44 +0800

    Create 集合.js

---
+++ 算法/集合.js
@@ -0,0 +1,75 @@
+//数据结构-集合 dsset
+//使用es6定义集合类
+
+class Dsset {
+    constructor(){
+    }
+    has(val){  //集合中存在某子集
+        return this.hasOwnProperty(val);
+    }
+    add(val){  //添加元素
+        if(!this.has(val)){
+            this[val] = val;
+        }
+    }
+    remove(val){  //删除元素
+        if(this.has(val)){
+            delete this[val];
+        }
+    }
+    clear(){ //清空所有元素
+        for(let key in this){
+            delete this[key];
+        }
+    }
+    size(){ //属性个数
+        return Object.keys(this).length;
+    }
+    values(){   //属性数组
+        return Object.keys(this);
+    }
+    union(set){    //并集
+        let result = new Dsset();
+        for(let key in this){
+            result.add(this[key]);
+        }
+        for(let key in set){
+            result.add(set[key]);
+        }
+        return result;
+    }
+    intersection(set){  //交集
+        let result = new Dsset();
+        for(let key in this){
+            if(set.has(key)){
+                result.add(key)
+            }
+        }
+        return result;
+    }
+    difference(set){   //差集
+        let result = new Dsset();
+        for(let key in this){
+            if(!set.has(key)){
+                result.add(key)
+            }
+        }
+        return result;
+    }
+    subset(set){   //子集
+        if(set.difference(this).size()){
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
+
+let set01 = new Dsset();
+let set02 = new Dsset();
+set01.add('a');
+set01.add('b');
+set01.add('c');
+set02.add('b');
+set02.add('a');
+console.log(set01.subset(set02));
