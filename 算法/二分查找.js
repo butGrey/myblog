3a3b939edbba6510ea8d553c4d699b9cd371c1bf
llbai <40267057+llbai@users.noreply.github.com>
2020-01-06 15:42:30 +0800

    Update 二分查找.js

--- 算法/二分查找.js
+++ 算法/二分查找.js
@@ -1,28 +1,51 @@
 /*查找旋转数组的最小值，如1234的旋转数组2341 3412 4123
 * 查找：顺序查找， 二分查找
 * */
-let arr = [1,2,3,4,5,6];
+let arr = [3,4,5,6,7,8,9,10,1,2];
 let res;
-midDivide(arr);
+res = midDivide(arr);
+// f(arr);
 console.log(res);
-function midDivide(arr) {
+function f(arr) {
     let mid  = Math.floor(arr.length/2);
     if (arr[mid]<arr[mid-1]&&arr[mid]<arr[mid+1]){
-        res = arr[mid]
+        res = arr[mid];
     } else if(arr[mid]>arr[mid-1]&&arr[mid]>arr[mid+1]){
-        res = arr[mid-1]<arr[mid+1]?arr[mid-1]:arr[mid+1];
+        res = arr[mid+1];
     } else{
         if(arr[mid-1]<arr[mid+1]&&arr[mid-1]<arr[arr.length-1]){
-            arr = arr.splice(0,mid);
+            arr = arr.slice(0,mid);
         }else{
-            arr = arr.splice(mid+1,arr.length);
+            arr = arr.slice(mid+1,arr.length);
         }
         if(arr.length==1){
             res = arr[0];
         }else if(arr.length==2){
             res = arr[0]<arr[1]?arr[0]:arr[1];
         }else {
-            midDivide(arr)
+            f(arr)
         }
     }
+}
+//二分查找、快速排序均使用指针
+function midDivide(arr) {
+    let low = 0,
+        height = arr.length-1;
+    while (low <= height){
+        let mid  = low + Math.floor((height-low)/2);
+        if(mid == low || mid == height){                        //边界值
+            return arr[mid]<arr[mid+1]?arr[mid]:arr[mid+1];
+        }else if (arr[mid]<arr[mid-1]&&arr[mid]<arr[mid+1]){    //mid是最小的
+            return arr[mid];
+        } else if(arr[mid]>arr[mid-1]&&arr[mid]>arr[mid+1]){    //mid是最大的
+            return arr[mid+1];
+        } else{                                                 //mid不大不小
+            if(arr[mid-1]<arr[mid+1]&&arr[mid-1]<arr[height]){
+                height = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+    }
+    return -1;
 }
