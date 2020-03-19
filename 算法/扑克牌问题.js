b6025007bab66ababe9da01c8831392fcbfb3a30
llbai <40267057+llbai@users.noreply.github.com>
2020-03-19 20:07:52 +0800

    Update 扑克牌问题.js

--- 算法/扑克牌问题.js
+++ 算法/扑克牌问题.js
@@ -3,18 +3,17 @@
     * */
     // let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13];
     let arr = [13,12,11,10,9,8,7,6,5,4,3,2,1];
-    let des = new Array(13);
+    let des = new Array(arr.length);
     let k = 1;
     function f(arr,i) {
-        let is = i;
+        let is = i-Math.pow(2,k-1);
         while (i>=0){
-            des[i] = arr[0];
-            arr.shift();
+            des[i] = arr.shift();
             i = i-Math.pow(2,k);
         }
         k++;
         if(arr.length){
-            f(arr,is-Math.pow(2,k-2))
+            f(arr,is)
         }
     }
     f(arr,12);
